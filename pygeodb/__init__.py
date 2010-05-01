463eae2a4b0aa3b1ab363bb125e0f602eb9940c2
Lukas Kolbe <lukas@einfachkaffee.de>
2010-05-02 03:37:29 +0800

    delete __future__ imports (python2.5 compatibility)
    Since most __future__ imports are either unused or unnecessary at this
    point, simply remove them. Allows pyGeoDb to run with python2.5 which is
    still widely deployed.

--- pygeodb/__init__.py
+++ pygeodb/__init__.py
@@ -7,10 +7,7 @@ Created by Maximillian Dornseif on 2010-01-16.
 Copyright (c) 2010 HUDORA. All rights reserved.
 """
 
-from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import division
-from future_builtins import map, filter, ascii, hex, oct
 
 from pygeodb.pygeodb_core import *
--- pygeodb/pygeodb_core.py
+++ pygeodb/pygeodb_core.py
@@ -27,10 +27,7 @@ locations = GeoLocation.select()
 distanz_liste = sort(locations, referenz)
 """
 
-from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import absolute_import
-from future_builtins import map, filter, ascii, hex, oct
 
 import math
 import operator
